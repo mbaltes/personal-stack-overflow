73d031b6d165b649df6c83560bf5ed67f3d5b23a
Mark Baltes <mark@mcb.me>
2019-07-18 11:49:22 -0700

    Added upvote view on questions with button to increase upvote count.

--- imports/ui/Question.jsx
+++ imports/ui/Question.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import Questions from '../api/questions';
+
 
 export default class Question extends Component {
   render() {
@@ -9,7 +11,17 @@ export default class Question extends Component {
           href={this.props.data.link} 
           target="_blank">{this.props.data.questionTitle}
         </a>
+        <button 
+          className="upvote-button" 
+          onClick={this.handleUpvote.bind(this)}
+        >
+          {this.props.data.upvotes}
+        </button>
       </article>
     );
+  }
+
+  handleUpvote() {
+    Questions.update({_id: this.props.data._id}, {$inc: {upvotes: 1}});
   }
 }
